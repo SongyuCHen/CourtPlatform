28797fe7291b45cf3d0c628777e26291d8bdcaa4
zceolrj <zceolrj@gmail.com>
2015-12-09 23:38:30 +0800

    add table T_Moodule

---
+++ CourtPlatform/src/nju/software/courtplatform/model/TModule.java
@@ -0,0 +1,77 @@
+package nju.software.courtplatform.model;
+
+public class TModule
+{
+	private int bh;
+	
+	private Integer levels;
+	
+	private String name;
+	
+	private String c_url;
+	
+	private Integer parent;
+	
+	private Integer xssx;
+
+	public int getBh()
+	{
+		return bh;
+	}
+
+	public void setBh(int bh)
+	{
+		this.bh = bh;
+	}
+
+	public Integer getLevels()
+	{
+		return levels;
+	}
+
+	public void setLevels(Integer levels)
+	{
+		this.levels = levels;
+	}
+
+	public String getName()
+	{
+		return name;
+	}
+
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+
+	public String getC_url()
+	{
+		return c_url;
+	}
+
+	public void setC_url(String c_url)
+	{
+		this.c_url = c_url;
+	}
+
+	public Integer getParent()
+	{
+		return parent;
+	}
+
+	public void setParent(Integer parent)
+	{
+		this.parent = parent;
+	}
+
+	public Integer getXssx()
+	{
+		return xssx;
+	}
+
+	public void setXssx(Integer xssx)
+	{
+		this.xssx = xssx;
+	}
+	
+}
