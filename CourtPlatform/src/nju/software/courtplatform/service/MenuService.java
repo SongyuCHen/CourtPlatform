f62cd04f4ebfb78481f1e0e7a9b2afdaa7c09807
lucyTheSlayer <saber_zyl@qq.com>
2015-12-15 20:13:45 +0800

    ~

--- CourtPlatform/src/nju/software/courtplatform/service/MenuService.java
+++ CourtPlatform/src/nju/software/courtplatform/service/MenuService.java
@@ -1,11 +1,16 @@
 package nju.software.courtplatform.service;
 
+import nju.software.courtplatform.dao.ModuleDao;
 import nju.software.courtplatform.help.MenuContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MenuService extends BaseService{
+	
+	@Autowired
+	private ModuleDao md;
 	
 	/**
 	 * 根据路径得到菜单上下文
