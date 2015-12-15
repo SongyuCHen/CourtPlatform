1ce7437d5163d3a1f1bf640c38de90596db160df
lucyTheSlayer <saber_zyl@qq.com>
2015-12-15 20:11:47 +0800

    ~

--- CourtPlatform/src/nju/software/courtplatform/controller/AppController.java
+++ CourtPlatform/src/nju/software/courtplatform/controller/AppController.java
@@ -1,5 +1,8 @@
 package nju.software.courtplatform.controller;
 
+import nju.software.courtplatform.service.MenuService;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -7,10 +10,15 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 @RequestMapping(value = "/app")
 public class AppController extends BaseController{
+	
+	@Autowired
+	private MenuService ms; 
+	
 	@RequestMapping(value = "/business")
 	public ModelAndView getPage(){
 		ModelAndView mv = new ModelAndView();
 		mv.setViewName("app/business");
+		mv.addObject("menuContext", ms.getMenuContext("app/business"));
 		return mv;
 	}
 
--- CourtPlatform/src/nju/software/courtplatform/service/MenuService.java
+++ CourtPlatform/src/nju/software/courtplatform/service/MenuService.java
@@ -1,8 +1,18 @@
 package nju.software.courtplatform.service;
 
+import nju.software.courtplatform.help.MenuContext;
+
 import org.springframework.stereotype.Service;
 
 @Service
 public class MenuService extends BaseService{
 	
+	/**
+	 * 根据路径得到菜单上下文
+	 * @param path    e.g.  app/business
+	 * @return
+	 */
+	public MenuContext getMenuContext(String path){
+		return null;
+	}
 }
